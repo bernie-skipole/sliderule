a0f7d46976b9446fb6031e0b565241bbbaf1b281
Bernard Czenkusz <bernie@skipole.co.uk>
2026-02-28 22:16:16 +0000

    further work on python program

--- imagemaker/byseven.py
+++ imagemaker/byseven.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     # right is False if the mid scale is moved to the left
     # hairline is the D value on which the hairline cursor is placed, or zero if no hairline is used 
 
-    rl = Rule(xvalue = 2.0, right = True, hairline=6.0)
+    rl = Rule(xvalue = 7.0, right = False, hairline=1.4)
 
     # Add a C scale
     rl.addCscale(100, midrule=20)  # indicates this scale is put on the middle rule 20 pixels down from the top of the middle rule. Rule at least 100 height
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     # Add a D scale
     rl.addDscale(120, btmrule=0) # indicates this scale is put on the bottom rule 0 pixels down from the top of the bottom rule. Rule at least 120 height
 
-    filename = "twoxthree.svg"
+    filename = "byseven.svg"
 
     rl.write(filename)
 
